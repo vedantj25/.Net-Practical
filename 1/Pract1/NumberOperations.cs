258ccd0eb91b761c2b77d37db0a93717ecfbec3d
Vedant Joshi <68338038+vedantj25@users.noreply.github.com>
2022-01-25 18:12:17 +0530

    Initial commit

---
+++ 1/Pract1/NumberOperations.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract1
+{
+    public class NumberOperations
+    {
+        public void arithmeticOperations(){
+            int a = 18;
+            int b = 6;
+
+            int c = a + b;
+            Console.WriteLine(c);
+
+            c = a - b;
+            Console.WriteLine(c);
+
+            c = a * b;
+            Console.WriteLine(c);
+
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        public void orderOfOperations()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+        }
+
+        public void precisionsAndLimits()
+        {
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+        }
+
+        public void doubleType()
+        {
+            double a = 19;
+            double b = 23;
+            double c = 8;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+        }
+
+        public void decimalType()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+        }
+
+        public void area()
+        {
+            double radius = 2.50;
+            double area = Math.PI * radius * radius;
+            Console.WriteLine(area);
+        }
+    }
+
+}
---
+++ 1/Pract1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Pract1
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            NumberOperations numberOperations = new NumberOperations();
+            BranchesAndLoops branchesAndLoops = new BranchesAndLoops();
+
+            numberOperations.arithmeticOperations();
+            Console.WriteLine("\n");
+            numberOperations.orderOfOperations();
+            Console.WriteLine("\n");
+            numberOperations.precisionsAndLimits();
+            Console.WriteLine("\n");
+            numberOperations.doubleType();
+            Console.WriteLine("\n");
+            numberOperations.decimalType();
+            Console.WriteLine("\n");
+            numberOperations.area();
+            Console.WriteLine("\n");
+            branchesAndLoops.ifCondition();
+            Console.WriteLine("\n");
+            branchesAndLoops.ifElseCondition();
+            Console.WriteLine("\n");
+            branchesAndLoops.whileDoWhileloops();
+            Console.WriteLine("\n");
+            branchesAndLoops.forLoop();
+            Console.WriteLine("\n");
+            branchesAndLoops.nestedLoops();
+            Console.WriteLine("\n");
+            branchesAndLoops.additionOfNums();
+
+        }
+    }
+}
